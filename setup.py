5c0e93391ff06a4bc130188af44eb93c6b51e5f8
spectraise <spectraise@gmail.com>
2021-05-04 00:17:41 +0300

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+def get_requirements():
+    with open('requirements.txt', 'r') as file:
+        requirements = file.read().split()
+        return [*requirements]
+
+
+setup(
+    name='rss-reader',
+    version='0.2',
+    description='',
+    author='Sergey Rutkovskiy',
+    url='https://github.com/spectraise/PythonFinalTask',
+    package='src',
+    install_requires=get_requirements(),
+    python_requires='>=3.8',
+    entry_points={
+        'console_scripts': [
+            'rss_reader = src.rss_reader:main',
+        ]
+    }
+)
