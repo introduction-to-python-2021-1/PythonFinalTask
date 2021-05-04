7a01f362d106ac22c724bdf406858405f94533fa
spectraise <spectraise@gmail.com>
2021-05-05 00:51:04 +0300

    Added package 'lxml' to requirements

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -7,7 +7,8 @@ setup(
     author='Sergey Rutkovskiy',
     url='https://github.com/spectraise/PythonFinalTask',
     packages=['components'],
-    install_requires=['bs4==0.0.1', 'beautifulsoup4==4.9.3', 'python-dateutil==2.8.1', 'requests==2.25.1'],
+    install_requires=['bs4==0.0.1', 'beautifulsoup4==4.9.3', 'lxml==4.6.3', 'python-dateutil==2.8.1',
+                      'requests==2.25.1'],
     python_requires='>=3.8',
     entry_points={
         'console_scripts': [
