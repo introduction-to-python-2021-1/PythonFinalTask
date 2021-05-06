f4f42457358f955bf25e22beb3ad7640ff7685db
SorryLegacy <alexremnev2@gmail.com>
2021-05-06 15:03:13 +0300

    Remnev Aleksandr

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+        name="rss_reader",
+        version='2.0',
+        description='',
+        py_modules=['rss_reader'],
+        author = 'Aleksandr Remnev',
+        author_email = 'alexremnev2@gmail.com',
+        packages=["rss_reader"],
+        zip_safe=False,
+        include_package_data=True,
+        entry_points={
+                'console_scripts': [
+                        'rss_reader=rss_reader:main',
+                ]
+        }
+)
