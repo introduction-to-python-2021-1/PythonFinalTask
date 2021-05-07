a0e2e714dc16b1a8cf86ef8d5b4e0d9113754e63
SorryLegacy <70037647+SorryLegacy@users.noreply.github.com>
2021-05-07 22:10:08 +0300

    Add files via upload

---
+++ rss_reader/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(
+        name="rss_reader",
+        version='2.0',
+        description='',
+        py_modules=['rss_reader'],
+        author = 'Aleksandr Remnev',
+        author_email = 'alexremnev2@gmail.com',
+        packages=find_packages(),
+        zip_safe=False,
+        include_package_data=True,
+        entry_points={
+                'console_scripts': [
+                        'rss_reader=rss_reader.rss_reader:main',
+                ],
+        }
+
+)
