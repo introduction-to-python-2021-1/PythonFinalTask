944089d4092a13c060e65343504946a9b75f70fe
Egor Makhlaev <egor.makhlaev@gmail.com>
2021-05-08 15:24:47 +0300

    1. Rename function process_response to parse_response in rss_reader.py    as now it's only parsing response 2. Add function set_limit to rss_reader.py that sets limit of output news 3. Create local_storage.py with LocalStorage class 4. Add --date argument to parser and corresponding logic for its processing 5. Add news.json and delete goodsample.xml as test logic will be changed

---
+++ rss_reader/rss_reader/local_storage.py
@@ -0,0 +1,9 @@
+class LocalStorage:
+    def __init__(self, name):
+        pass
+
+    def set_channel(self, url, channel):
+        pass
+
+    def get_channel(self, url, channel):
+        pass
--- rss_reader/rss_reader/rss_reader.py
+++ rss_reader/rss_reader/rss_reader.py
@@ -9,10 +9,14 @@ from urllib.error import HTTPError
 from urllib.request import urlopen
 import xml.etree.ElementTree as ET
 
+from local_storage import LocalStorage
+
 VERSION = "2.0"
 
 logging.basicConfig(level=logging.ERROR, format="%(message)s")
 logger = logging.getLogger()
+
+local_storage = LocalStorage("localstorage")
 
 
 def get_response(url):
@@ -40,13 +44,12 @@ def get_response(url):
     return response
 
 
-def process_response(response, limit):
+def parse_response(response):
     """
-    Returns dictionary with channel title and items. Items number is determined by provided limit argument.
+    Returns dictionary with channel title and items.
 
             Parameters:
                     response (http.client.HTTPResponse): Response from server provided by get_response function
-                    limit (None) or (int): Max number of items in result dictionary
 
             Returns:
                     {"Title": (str), "Items": (list)}: Dictionary with channel title and items
@@ -67,10 +70,7 @@ def process_response(response, limit):
     channel_title = root.findtext("channel/title")
     channel_items = []
 
-    for number_of_news_item, news_item in enumerate(
-        islice(root.iterfind("channel/item"), 0, max(0, limit) if limit is not None else limit)
-    ):
-
+    for number_of_news_item, news_item in enumerate(root.iterfind("channel/item")):
         logger.info(f"Process item № {number_of_news_item + 1}")
 
         channel_items.append({
@@ -80,6 +80,24 @@ def process_response(response, limit):
         })
 
     return {"Title": root.findtext("channel/title"), "Items": channel_items}
+
+
+def set_limit(channel, limit):
+    """
+    Returns dictionary with channel title and items. Items number is determined by provided limit argument.
+
+            Parameters:
+                    channel {"Title": (str), "Items": (list)}: Dictionary with channel title and items
+                    limit (None) or (int): Max number of items in result dictionary, if (None) all items are included
+
+            Returns:
+                    {"Title": (str), "Items": (list)}: Dictionary with channel title and items
+    """
+    logger.info(f"Limit output to {limit or len(channel['Items'])} news")
+
+    channel["Items"] = channel["Items"][:limit]
+
+    return channel
 
 
 def print_news(channel):
@@ -125,14 +143,30 @@ def main(argv=sys.argv):
     parser.add_argument("--json", action="store_true", help="Print result as JSON in stdout")
     parser.add_argument("--verbose", action="store_true", help="Outputs verbose status messages")
     parser.add_argument("--limit", type=int, help="Limit news topics if this parameter provided")
+    parser.add_argument("--date", type=str, help="Return news topics which were published in specific date")
 
     args = parser.parse_args(argv[1:])
 
     if args.verbose:
         logger.setLevel(logging.INFO)
 
-    response = get_response(args.source)
-    channel_info_and_items = process_response(response, args.limit)
+    if args.date:
+
+        if args.source:
+            channel_info_and_items = local_storage.get_channel(args.source, args.date)
+        else:
+            channel_info_and_items = local_storage.get_channel(None, args.date)
+
+        if channel_info_and_items is None:
+            logger.error("Couldn't find news topics which were published in specific date")
+            sys.exit()
+
+    else:
+        response = get_response(args.source)
+        channel_info_and_items = parse_response(response)
+        local_storage.set_channel(args.source, channel_info_and_items)
+
+    set_limit(channel_info_and_items, args.limit)
     print_news(channel_info_and_items)
 
     if args.json:
