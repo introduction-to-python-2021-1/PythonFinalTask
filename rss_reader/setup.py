d7bb06c93b4b42b72dce0950609c5b94ede356e0
spectraise <spectraise@gmail.com>
2021-05-09 18:41:27 +0300

    Reworked project structure
    1. Module 'rss_reader' moved to package 'rss_reader'
    2. Added package 'rss_reader' to setup.py

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -6,9 +6,9 @@ setup(
     description='Command-line RSS reader',
     author='Sergey Rutkovskiy',
     url='https://github.com/spectraise/PythonFinalTask',
-    packages=['components'],
+    packages=['rss_reader', 'components', ],
     install_requires=['bs4==0.0.1', 'beautifulsoup4==4.9.3', 'lxml==4.6.3', 'python-dateutil==2.8.1',
-                      'requests==2.25.1'],
+                      'requests==2.25.1', ],
     python_requires='>=3.8',
     entry_points={
         'console_scripts': [
