59507b06e9cb004b6e3ecea667f3d3b8f271ebd2
Egor Makhlaev <egor.makhlaev@gmail.com>
2021-05-09 22:38:36 +0300

    Add functionality in set_channel_by_url method of LocalStorage class in local_storage.py

--- rss_reader/rss_reader/local_storage.py
+++ rss_reader/rss_reader/local_storage.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-from dateparser import parse
+import dateparser
 
 
 class LocalStorage:
@@ -14,10 +14,27 @@ class LocalStorage:
         self.jsonpath = jsonpath
 
     def set_channel_by_url(self, url, channel):
+        def parse_date_from_news_item(news_item):
+            return dateparser.parse(news_item["Date"])
+        # Sorts channel news items by date
+        # Changes input dictionary "channel", not creates a copy of it
+        channel["Items"] = sorted(channel["Items"], reverse=True, key=parse_date_from_news_item)
+
         storage_content = self.read_from_storage_file()
 
         if url in storage_content:
-            pass
+            # To prevent duplication of news items from the same url
+            i = 0
+            tmp_list = []
+            storage_content_has_news_items = bool(len(storage_content[url]["Items"]))
+            date_of_latest_news_item_in_storage_content = parse_date_from_news_item(storage_content[url]["Items"][0])
+
+            while i < len(channel["Items"]) and storage_content_has_news_items and \
+                parse_date_from_news_item(channel["Items"][i]) > date_of_latest_news_item_in_storage_content:
+                    tmp_list.append(channel["Items"][i])
+                    i += 1
+
+            storage_content[url]["Items"] = tmp_list + storage_content[url]["Items"]
         else:
             storage_content[url] = channel
 
@@ -31,6 +48,3 @@ class LocalStorage:
 
     def write_to_storage_file(self, data):
         self.jsonpath.write_text(json.dumps(data, indent=4))
-
-
-local_storage = LocalStorage("localstorage")
