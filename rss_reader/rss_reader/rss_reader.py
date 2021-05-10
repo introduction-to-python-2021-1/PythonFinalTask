68dd0bd4f5bb359efb3659f8ffb64cf368ecda2c
spectraise <spectraise@gmail.com>
2021-05-11 00:12:50 +0300

    Added error for the case when the specified URL does not contain RSS

--- rss_reader/rss_reader/rss_reader.py
+++ rss_reader/rss_reader/rss_reader.py
@@ -20,7 +20,7 @@ def main(argv=sys.argv[1:]):
         logging.basicConfig(level=logging.ERROR)
     logger = logging
     if isinstance(args.limit, int) and args.limit < 1:
-        logger.error(f' The limit argument must be greater than zero ({args.limit} was passed).')
+        logger.error(f' The limit argument must be greater than zero ({args.limit} was passed)')
         sys.exit()
     if args.source:
         try:
@@ -32,15 +32,18 @@ def main(argv=sys.argv[1:]):
         else:
             logger.info(' Parsing XML from the specified URL')
             soup = BeautifulSoup(response.content, 'lxml-xml')
-            feed_title = soup.find('title').text
-            items = soup.find_all('item')
-            logger.info(f' Founded {len(items)} news items')
-            if items:
-                feed = Feed(feed_title, items, args.json, logger, args.limit)
-                print(feed)
-            logger.info(' Successfully completed')
+            if soup.find('rss'):
+                feed_title = soup.find('title').text
+                items = soup.find_all('item')
+                logger.info(f' Founded {len(items)} news items')
+                if items:
+                    feed = Feed(feed_title, items, args.json, logger, args.limit)
+                    print(feed)
+                logger.info(' Successfully completed')
+            else:
+                logger.error(' Specified URL does not contain RSS. Please check the specified URL and try again')
     else:
-        logger.error(' Source URL not specified. Please check your input and try again.')
+        logger.error(' Source URL not specified. Please check your input and try again')
 
 
 if __name__ == '__main__':
