1849e4bb3e0992bf77202a1b0a8a9312a772eb14
SorryLegacy <alexremnev2@gmail.com>
2021-05-15 15:23:32 +0300

    Fix

--- rss_reader/rss_reader/rss_reader.py
+++ rss_reader/rss_reader/rss_reader.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import logging
 import json
+from urllib.error import URLError
+from math import inf
 
 import feedparser
 
@@ -10,7 +12,7 @@ logger = logging.getLogger()
 
 
 def create_parser(args):
-    """create positional and optional arguments"""
+    """ Create positional and optional arguments """
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--version", action="version", help="Print version info", version="Version 2.0")
     parser.add_argument("url", type=str, help="URL ")
@@ -22,34 +24,47 @@ def create_parser(args):
 
 
 def open_url(url):
-    """ try to open url """
+    """ try to open url
+    :param url page
+    :return feed of news"""
+
     try:
         logger.info(f"open {url} and start parse")
         feed = feedparser.parse(url)
-    except SystemExit:
-        logger.error(f"cant open {url}")
+    except URLError:
+        logger.error(f"cant open or found  {url}")
+        sys.exit()
+    except Exception:
+        logger.error(f"exception: {Exception}")
         sys.exit()
 
     return feed
 
 
 def print_news(args):
-    """Check format and print news on console or json"""
+    """Check format and print news on console or json
+    :param args from the user"""
     feed = open_url(args.url)
     try:
         if not args.json:
             print(f'Feed: {feed["channel"]["title"]}')
         else:
             print("JSON")
-            with open("data_file.json", "w") as f:
-                json.dump(feed["channel"]["title"], f, indent=3)
+            print(json.dumps(feed["channel"]["title"], indent=3))
 
-    except SystemExit:
+    except Exception:
         logger.error(f"it's not rss format")
         sys.exit()
 
     make_dict = {}
     count = 0
+
+    if not args.limit:
+        args.limit = 50
+        print(f"You enter nothing or 0 and ")
+    elif args.limit < 0:
+        print(f"negative limit is entered so it displays all available news.(You enter limit = {args.limit} ")
+        args.limit = 50
 
     for item in feed["items"][:args.limit]:
         logger.info(f"Process item № {count + 1}")
@@ -57,9 +72,8 @@ def print_news(args):
         make_dict["PubDate"] = item['published']
         make_dict["Link"] = item["link"]
         if args.json:
-            with open("data_file.json", "a") as f:
-                json.dump(make_dict, f, indent=3)
-                count += 1
+            print(json.dumps(make_dict, indent=3))
+            count += 1
 
         else:
 
@@ -69,7 +83,9 @@ def print_news(args):
 
 
 def main():
+
     args = create_parser(sys.argv[1:])
+
     if args.verbose:
         logger.setLevel(logging.INFO)
     print_news(args)
--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,6 @@
 import io
 import unittest
+from urllib.error import URLError
 from rss_reader import rss_reader
 import sys
 
@@ -10,24 +11,48 @@ class TestProcessResponse(unittest.TestCase):
         sys.stdout = self.out
 
     def test_limit(self):
+        """test positive limit"""
         parser = rss_reader.create_parser(["--limit 5"])
         self.assertTrue(parser)
 
+    def test_zero_limit(self):
+        """Test zero limit"""
+        parser = rss_reader.create_parser(["--limit 0"])
+        self.assertTrue(parser)
+
     def test_url(self):
+        """Test url page"""
         parser = rss_reader.create_parser(["https://news.yahoo.com/rss/"])
         self.assertTrue(rss_reader.open_url(parser.url))
 
+    def test_bad_url(self):
+        """Try test bad urs page"""
+        parser = rss_reader.create_parser(["https://newsyahoo.com/rss/"])
+        with self.assertRaises(URLError):
+            self.assertEqual(rss_reader.open_url(parser.url), f"cant open or found {parser.url}")
+
+
     def test_version_none_argyment(self):
+        """Test version with out url"""
         with self.assertRaises(SystemExit):
             parser = rss_reader.create_parser([None, "--version"])
 
         self.assertEqual(self.out.getvalue(), "Version 2.0\n")
 
+    def test_version_url(self):
+        """Test version with url"""
+        with self.assertRaises(SystemExit):
+            parser = rss_reader.create_parser(["https://news.yahoo.com/rss/", "--version"])
+        self.assertEqual(self.out.getvalue(), "Version 2.0\n")
+
+
     def test_json(self):
+        """test JSON output"""
         parser = rss_reader.create_parser(["https://news.yahoo.com/rss/", "--json"])
         self.assertTrue(parser.json)
 
     def test_verbose(self):
+        """Test log"""
         parser = rss_reader.create_parser(["https://news.yahoo.com/rss/", "--verbose"])
         self.assertTrue(parser.verbose)
 
