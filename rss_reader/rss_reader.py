5cd3d1b46ad7b16038254f4d8996fd98821023ee
Bulachka <bella-divina@yandex.by>
2021-05-15 17:24:27 +0200

    refactor: parse news in separate functions

--- rss_reader/rss_reader.py
+++ rss_reader/rss_reader.py
@@ -11,6 +11,32 @@ from urllib.error import URLError
 
 import feedparser
 
+NEWS_PARTS = ["title", "published", "summary", "description", "storyimage", "media_content", "link"]
+
+
+def printing_parsing_news(content, number_of_news_to_show):
+    # Print news to stdout
+    for news in content.entries[:number_of_news_to_show]:
+        for item in NEWS_PARTS:
+            if item in news.keys():
+                print(item.capitalize() + ": " + str(news[item]))
+        print("***************\n")
+
+
+def printing_parsing_news_in_json(content, number_of_news_to_show):
+    # Convert news to json format and print them
+    # logger.info(f"Convert news in json format")
+    json_dict = {}
+    newslist = []
+    newsdict = {}
+
+    for news in content.entries[:number_of_news_to_show]:
+        for item in NEWS_PARTS:
+            if item in news.keys():
+                json_dict[item.capitalize()] = news[item]
+        newslist.append(json_dict.copy())
+    newsdict["news"] = newslist
+    print(jsn.dumps(newsdict, indent=1))
 
 def open_rss_link(source, limit, json, verbose):
     """
@@ -54,76 +80,9 @@ def open_rss_link(source, limit, json, verbose):
         number_of_news_to_show = len(content.entries)
 
     if json:
-        # Convert news to json format
-        logger.info(f"Convert news in json format")
-        json_dict = {}
-        newslist = []
-        newsdict = {}
-
-        for news in content.entries[:number_of_news_to_show]:
-            if "title" in news.keys():
-                json_dict["Title"] = news.title
-
-            if "summary" in news.keys():
-                json_dict["Summary"] = news.summary
-
-            if "description" in news.keys():
-                json_dict["Summary"] = news.description
-
-            if "published" in news.keys():
-                json_dict["Date"] = news.published
-
-            if "storyimage" in news.keys():
-                json_dict["Main_image"] = news.storyimage
-
-            if "media_content" in news.keys():
-                json_dict["Image"] = news.media_content
-
-            if "tags" in news.keys():
-                if "term" in news.tags:
-                    json_dict["Tags"] = news.tags[0]["term"]
-
-            if "link" in news.keys():
-                json_dict["News link"] = news.link
-
-            newslist.append(json_dict.copy())
-
-        newsdict["news"] = newslist
-
-        print(jsn.dumps(newsdict, indent=1))
-
+        printing_parsing_news_in_json(content, number_of_news_to_show)
     else:
-        # Print news to stdout
-        for news in content.entries[:number_of_news_to_show]:
-            if "title" in news.keys():
-                print("\n")
-                print(f"Title: {news.title}")
-
-            if "published" in news.keys():
-                print(f"Date: {news.published}")
-
-            if "summary" in news.keys():
-                print(f"Summary: {news.summary}")
-
-            if "description" in news.keys():
-                print(f"Summary: {news.description}")
-                print("\n")
-
-            if "storyimage" in news.keys():
-                print("\n\n")
-                print(f"Main_image: {news.storyimage}")
-
-            if "media_content" in news.keys():
-                print("\n\n")
-                print(f"Image: {news.media_content}")
-
-            if "tags" in news.keys():
-                if "term" in news.tags:
-                    print(f"Tags: {news.tags[0]}")
-
-            if "link" in news.keys():
-                print(f"Link: {news.link}")
-                print("****************************************")
+        printing_parsing_news(content, number_of_news_to_show)
 
     logger.info(f"End of reading")
 
