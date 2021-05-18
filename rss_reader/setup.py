d1f82b0a010a4d876c4648327e30fa1f281f1e09
YuliyaLit <Yuliya-litvinko@mail.ru>
2021-05-18 22:23:48 +0300

    init

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="rss_reader",
-    version="1.0.1",
+    version="1.0",
     description="Pure Python command-line RSS reader.",
     url="https://github.com/YuliyaLit/PythonFinalTask",
     author="Stralchuk Yuliya",
     author_email='Yuliya-litvinko@mail.ru',
     keywords="rss_reader",
     packages=["rss_reader"],
-    install_requires=['feedparser==6.0.2'],
+    install_requires=['feedparser'],
     entry_points={
         "console_scripts": [
             "rss_reader=rss_reader.rss_reader:main",
