559da3086b1adf2cca00448e0232eafa4ee1a8bf
bobich.m <mbobich@marathonbet.ru>
2021-05-19 12:56:56 +0300

    Old structure was deleted

--- rss_reader/tests/tests_for_news_processor.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from rss_reader.rss_reader.reader_core.news_processor import NewsProcessor
-
-
-class TestNewsProcessor(unittest.TestCase):
-    def test_get_news_with_empty_parser(self):
-        proc = NewsProcessor(1)
-        proc.get_news("https://news.yahoo.com/rss/")
