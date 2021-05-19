69f23728a907ba2db0d32f8a49ccfd2cfca5f768
Bulachka <bella-divina@yandex.by>
2021-05-19 21:48:32 +0200

    refactor: move test variable in a separate file

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,44 +3,10 @@ from unittest import mock
 from unittest.mock import patch
 
 import feedparser
+import test_data as td
 from rss_reader import rss_reader as rs
 
 NEWSLINK = "https://news.yahoo.com/rss/"
-TEST_ENTRIES = [{'title': 'On-duty police officer sexually assaulted by gas station manager, Georgia cops say',
-                 'title_detail': {'type': 'text/plain', 'language': None, 'base': 'https://news.yahoo.com/rss/',
-                                  'value': 'On-duty police officer sexually assaulted by gas station manager, Georgia cops say'},
-                 'links': [{'rel': 'alternate', 'type': 'text/html',
-                            'href': 'https://news.yahoo.com/duty-police-officer-sexually-assaulted-010634049.html'}],
-                 'link': 'https://news.yahoo.com/duty-police-officer-sexually-assaulted-010634049.html',
-                 'published': '2021-05-18T01:06:34Z',
-                 'source': {'href': 'https://www.kentucky.com/', 'title': 'Lexington Herald-Leader'},
-                 'id': 'duty-police-officer-sexually-assaulted-010634049.html', 'guidislink': False, 'media_content': [
-        {'height': '86',
-         'url': 'https://s.yimg.com/uu/api/res/1.2/A3riyROEGQuSpO0M838c0g--~B/aD02NDE7dz0xMTQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/lexington_herald_leader_mcclatchy_articles_314/d453d37647ec075638a8bc71a3e80ce0',
-         'width': '130'}], 'media_credit': [{'role': 'publishing company'}], 'credit': ''},
-                {'title': 'Heroic Dog Who Lost Her Snout Saving Two Girls Years Ago Passes Away in the Philippines',
-                 'title_detail': {'type': 'text/plain', 'language': None, 'base': 'https://news.yahoo.com/rss/',
-                                  'value': 'Heroic Dog Who Lost Her Snout Saving Two Girls Years Ago Passes Away in the Philippines'},
-                 'links': [{'rel': 'alternate', 'type': 'text/html',
-                            'href': 'https://news.yahoo.com/heroic-dog-lost-her-snout-151239087.html'}],
-                 'link': 'https://news.yahoo.com/heroic-dog-lost-her-snout-151239087.html',
-                 'published': '2021-05-18T15:12:39Z',
-                 'source': {'href': 'https://nextshark.com/', 'title': 'NextShark'},
-                 'id': 'heroic-dog-lost-her-snout-151239087.html', 'guidislink': False, 'media_content': [
-                    {'height': '86',
-                     'url': 'https://s.yimg.com/uu/api/res/1.2/rSALC6BHE5GLgr.puYJEuA--~B/aD00MjU7dz04MDA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/nextshark_articles_509/1939d15898adc9f02b3aa5266f51881a',
-                     'width': '130'}], 'media_credit': [{'role': 'publishing company'}], 'credit': ''},
-                {'title': '600,000 kids between 12 and 15 have received Pfizer vaccine dose since FDA authorization',
-                 'title_detail': {'type': 'text/plain', 'language': None, 'base': 'https://news.yahoo.com/rss/',
-                                  'value': '600,000 kids between 12 and 15 have received Pfizer vaccine dose since FDA authorization'},
-                 'links': [{'rel': 'alternate', 'type': 'text/html',
-                            'href': 'https://news.yahoo.com/600-000-kids-between-12-151913054.html'}],
-                 'link': 'https://news.yahoo.com/600-000-kids-between-12-151913054.html',
-                 'published': '2021-05-18T15:19:13Z', 'source': {'href': 'https://www.axios.com/', 'title': 'Axios'},
-                 'id': '600-000-kids-between-12-151913054.html', 'guidislink': False, 'media_content': [{'height': '86',
-                                                                                                         'url': 'https://s.yimg.com/uu/api/res/1.2/u194wekMUQEk_9oy8lv3Iw--~B/aD03MjA7dz0xMjgwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/axios_articles_623/0ed6f0918ef60763f094e36c4bcc43f4',
-                                                                                                         'width': '130'}],
-                 'media_credit': [{'role': 'publishing company'}], 'credit': ''}]
 
 
 class TestMainReader(unittest.TestCase):
@@ -83,7 +49,7 @@ class TestMainReader(unittest.TestCase):
     def test_limit_really_limit_one(self, mock_print):
         # Test number of output lines is equal limit * 5 (number of lines in one news WITHOUT logs no json)
         content = mock.MagicMock()
-        content.entries = TEST_ENTRIES
+        content.entries = td.TEST_ENTRIES
         rs.printing_parsing_news(content, 1)
         news = mock_print.call_args_list
         self.assertEqual(len(news), (1 * 5) + 1)
@@ -92,7 +58,7 @@ class TestMainReader(unittest.TestCase):
     def test_limit_really_three(self, mock_print):
         # Test number of output lines is equal limit * 5 (number of lines in one news WITHOUT logs no json)
         content = mock.MagicMock()
-        content.entries = TEST_ENTRIES
+        content.entries = td.TEST_ENTRIES
         rs.printing_parsing_news(content, 3)
         news = mock_print.call_args_list
         self.assertEqual(len(news), (3 * 5) + 1)
@@ -102,7 +68,7 @@ class TestMainReader(unittest.TestCase):
     def test_printing_parsing_news_in_json(self, mock_print):
         # Test output have the first key of our json dictionary ("news"), which is not present in the regular output
         content = mock.MagicMock()
-        content.entries = TEST_ENTRIES
+        content.entries = td.TEST_ENTRIES
         rs.printing_parsing_news_in_json(content, 1)
         first_new = mock_print.call_args_list[0].args[0]
         self.assertTrue("news" in first_new)
@@ -111,7 +77,7 @@ class TestMainReader(unittest.TestCase):
     def test_limit_is_not_passed(self):
         # Test case user do not pass any limit
         content = mock.MagicMock()
-        content.entries = TEST_ENTRIES
+        content.entries = td.TEST_ENTRIES
         limit = None
         number_of_news_to_show = rs.set_limit(content, limit)
         self.assertEqual(number_of_news_to_show, len(content.entries))
@@ -119,7 +85,7 @@ class TestMainReader(unittest.TestCase):
     def test_limit_is_small(self):
         # Test case user pass limit that smaller than total number of news (3)
         content = mock.MagicMock()
-        content.entries = TEST_ENTRIES
+        content.entries = td.TEST_ENTRIES
         limit = 2
         number_of_news_to_show = rs.set_limit(content, limit)
         self.assertEqual(number_of_news_to_show, 2)
@@ -128,7 +94,7 @@ class TestMainReader(unittest.TestCase):
         # Test case user pass limit that bigger than total number of news (3), should set number_of_news_to_show as
         # maximum, means len(content.entries)
         content = mock.MagicMock()
-        content.entries = TEST_ENTRIES
+        content.entries = td.TEST_ENTRIES
         limit = 100500
         number_of_news_to_show = rs.set_limit(content, limit)
         self.assertEqual(number_of_news_to_show, len(content.entries))
@@ -136,7 +102,7 @@ class TestMainReader(unittest.TestCase):
     def test_limit_is_invalid(self):
         # Test case user pass 0 or negative int, should print user-friendly message and exit
         content = mock.MagicMock()
-        content.entries = TEST_ENTRIES
+        content.entries = td.TEST_ENTRIES
         limit = -1
         with self.assertRaises(SystemExit):
             rs.set_limit(content, limit)
