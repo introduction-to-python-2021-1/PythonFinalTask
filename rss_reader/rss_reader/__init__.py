de12c348e0ced65c8ee462c91300d5268b2b0b85
SorryLegacy <70037647+SorryLegacy@users.noreply.github.com>
2021-05-20 00:13:20 +0300

    Update __init__.py

--- rss_reader/rss_reader/__init__.py
+++ rss_reader/rss_reader/__init__.py
@@ -1,1 +1,4 @@
-import rss_reader
+from rss_reader import *h test_data
+sorrylegacy@sorrylegacy-MS-7996:~/Рабочий стол/final /PythonFinalTask/tests$ touch test_data.py
+sorrylegacy@sorrylegacy-MS-7996:~/Рабочий стол/final /PythonFinalTask/tests$ 
+
