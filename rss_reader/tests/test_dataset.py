d0f715cae1f8920f764a06a8294f20ecd035a956
SorryLegacy <70037647+SorryLegacy@users.noreply.github.com>
2021-05-20 14:20:31 +0300

    Update test_dataset.py

--- rss_reader/tests/test_dataset.py
+++ rss_reader/tests/test_dataset.py
@@ -15,6 +15,13 @@ class TestProcessResponse(unittest.TestCase):
         with self.assertWarns(ResourceWarning):
             self.test.__init__()
             self.assertTrue(self.test)
+            
+    def test_file_clear(self):
+        self.assertEqual(os.path.getsize("data.csv"), 0)
+                            
+    def test_file(self):
+        """File open"""
+        self.assertTrue(open("data.csv"))
 
     def test_dataframe(self):
         with self.assertRaises(IndexError):
