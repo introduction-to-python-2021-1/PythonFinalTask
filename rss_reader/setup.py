48c5a91371a69bd00143a68f9defea5aa003d1f0
Viktor Kolesnikov <mack123456789@mail.ru>
2021-05-20 14:27:54 +0300

    Fix pycodestyle. Add requests module to requires

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -6,9 +6,9 @@ setup(
     description='Command-line RSS reader',
     author='Kolesnikov Viktor',
     url='https://github.com/MackDillan/PythonFinalTask',
-    install_requires=['beautifulsoup4', 'lxml'],
+    install_requires=['beautifulsoup4', 'lxml', 'requests'],
     python_requires='>=3.8',
-    packages=['rss_reader'],    
+    packages=['rss_reader'],
     entry_points={
         'console_scripts': [
             'rss_reader=rss_reader.rss_reader:main',
