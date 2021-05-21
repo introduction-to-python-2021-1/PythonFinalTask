8cc106f9177e6ec63a21d5569dbe65afc289052b
SorryLegacy <alexremnev2@gmail.com>
2021-05-21 15:45:58 +0300

    fix

--- rss_reader/tests/test_dataset.py
+++ rss_reader/tests/test_dataset.py
@@ -1,5 +1,10 @@
 import os
 import unittest
+import io
+import sys
+
+import pandas as pd
+
 try:
     from dataset import Data
 except ImportError:
@@ -62,6 +67,28 @@ class TestLimit(unittest.TestCase):
 
     def tearDown(self):
         os.remove("data.csv")
+
+
+class TestPrint(unittest.TestCase):
+    def setUp(self):
+        link_0 = 'https://news.yahoo.com/sheila-bridges-design-notes-122057672.html'
+        link_1 = 'https://news.yahoo.com/george-floyd-square-1-later-192543800.html'
+        link_2 = 'https://news.yahoo.com/ads-2021-hotel-awards-011339231.html'
+        data = {"Date": ['2021-05-19T12:20:57Z', '2021-05-20T19:25:43Z', '2021-05-21T08:10:55Z'],
+                "Link": [link_0, link_1, link_2],
+                "Title": ['Sheila Bridges Design Notes', 'George Floyd Square 1 year later',"AD's 2021 Hotel Awards"]
+                }
+        self.test = pd.DataFrame(data)
+        self.out = io.StringIO()
+        sys.stdout = self.out
+
+    def test_print(self):
+        with self.assertWarns(ResourceWarning):
+            self.ans = Data()
+        self.ans.make_csv()
+        self.ans.print_data(20210520, 2, None)
+        self.assertTrue(self.out.getvalue())
+
 
 if __name__ == "__main__":
     unittest.main()
