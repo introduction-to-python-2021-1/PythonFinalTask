9bda4f71b23295edd1525c059de06fe006c268c3
bobich.m <mbobich@marathonbet.ru>
2021-05-23 23:04:05 +0300

    Test tests/tests_for_reader.py configuration

--- rss_reader/tests/tests_for_reader.py
+++ rss_reader/tests/tests_for_reader.py
@@ -3,25 +3,16 @@
 """
 import unittest
 import requests
-from unittest.mock import patch
 from unittest.mock import MagicMock
-from io import StringIO
 from rss_core.reader import SiteReader
 
 
 class TestReader(unittest.TestCase):
     """ Tests for SiteReader """
 
-    def test_get_data_empty_link(self):
+    def test_bad_response_code(self):
+        response = requests.Response()
+        response.status_code = 404
+        requests.get = MagicMock(return_value=response)
         reader = SiteReader()
-        reader.get_data("https://")
-
-
-    # @patch('sys.stdout', new_callable=StringIO)
-    # def test_get_data_empty_link(self, mock_stdout):
-    #     reader = SiteReader()
-    #     with self.assertRaises(SystemExit) as cm:
-    #         reader.get_data()
-    #     self.assertEqual(cm.exception.code, 1)
-    #     self.assertEqual("[ERROR] Value error has occurred while getting data from chanel",
-    #                      mock_stdout.getvalue().strip())
+        self.assertRaises(ConnectionError, reader.get_data, "http://")
