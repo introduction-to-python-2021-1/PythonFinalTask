f8372e46998492cd2734615a7043bc47900a2035
vladshe <vladsheydenkv@gmail.com>
2021-05-23 23:42:06 +0300

    add news caching

--- rss_reader/rss_reader.py
+++ rss_reader/rss_reader.py
@@ -6,6 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 import argparse
 import logging
+import os
 
 
 def create_root_logger():
@@ -73,8 +74,39 @@ def get_args():
                         help='Prints version and stop',
                         )
 
+    parser.add_argument('--date',
+                        type = str,
+                        help = 'Return news with the specified data',
+                        default = '',
+                        )
+
     args = parser.parse_args()
     return args
+
+
+def get_feed_from_cache(date, limit):
+    file_path = os.path.abspath(os.path.dirname('app'))
+    file_path += os.path.sep + 'feed_cache' + os.path.sep + date + '.json'
+    try:
+        with open(file_path) as fp:
+            news = json.load(fp)
+            return news[:limit]
+    except FileNotFoundError:
+        print('Can\'t find news with this date')
+    return False
+
+
+def print_cached_feed(feed_list):
+    for feed in feed_list:
+        loaded_feed = json.loads(feed)
+        print(loaded_feed.keys())
+        print(loaded_feed['title'])
+        print(loaded_feed['link'])
+        print(time.strftime("%y-%m-%d %H:%M", tuple(loaded_feed['date'])))
+        if loaded_feed['img']:
+            print(loaded_feed['img'])
+        if loaded_feed['content']:
+            print(loaded_feed['content'])
 
 
 class RssParser:
@@ -96,23 +128,47 @@ class RssParser:
         for feed in data['entries'][:self.limit]:
             title = feed.get('title', 'Absence of title')
             link = feed.get('link', 'Absence of link')
-            date = time.strftime('%y-%m-%d %H:%M', feed.get('published_parsed'))
+            date = feed.get('published_parsed')
             img = get_img(link)
             summary_list = []
             if feed.get('summary'):
                 summary_list.append(feed.get('summary'))
             item = collections.namedtuple('item', 'title, link, date, img, content')._make((title, link, date, img, summary_list))
+            self.save_feed_into_cache(item)
             self.items.append(item)
 
-    def convert_to_json(self):
+    def convert_to_json(self, item):
         return json.dumps({'url': self.url,
                            'feed': {'name': self.name,
                                     'items': [item._asdict() for item in self.items]}}, ensure_ascii=False)
 
+    def save_feed_into_cache(self, item):
+        date = time.strftime('%Y%m%d', item.date)
+        dir_path = os.path.abspath(os.path.dirname('app')) + os.path.sep + 'feed_cache' + os.path.sep
+        if not os.path.exists(dir_path):
+            os.mkdir(dir_path)
+        file_path = dir_path + date + '.json'
+        item_to_cache = json.dumps(item._asdict(), ensure_ascii=False)
+        print('trying to open')
+        with open(file_path, 'a', encoding='utf-8') as fp:
+            if os.stat(file_path).st_size == 0:
+                feed_list = [item_to_cache]
+                json.dump(feed_list, fp)
+                return
+        with open(file_path, 'r', encoding='utf-8') as fp:
+            feed_list = json.load(fp)
+            if item_to_cache in feed_list:
+                return
+            feed_list.append(item_to_cache)
+        with open(file_path, 'w', encoding='utf-8') as fp:
+            json.dump(feed_list, fp)
+        return
+
+
     def __str__(self):
         result_str = self.name + '\n'
         for item in self.items:
-            item_as_str = f'Title: {item.title}\nLink: {item.link}\nDate: {item.date}\n'
+            item_as_str = f'Title: {item.title}\nLink: {item.link}\nDate: {time.strftime("%y-%m-%d %H:%M", item.date)}\n'
             result_str += item_as_str
             if item.content:
                 result_str += 'Content: '
@@ -142,20 +198,23 @@ def main():
         raise ValueError("URL is empty, please input URL")
 
     logger.info(f'Program started, url: {arguments.url}')
-    get_data = RssParser(arguments.url, arguments.limit)
-    try:
-        get_data.get_feed()
-    except Exception as error_message:
-        logger.error(error_message)
-        if not arguments.verbose:
-            print(error_message)
 
-    if arguments.json:
-        print(get_data.convert_to_json())
+    if arguments.date:
+        feed_list = get_feed_from_cache(arguments.date, arguments.limit)
+        print_cached_feed(feed_list)
     else:
-        print(get_data)
-    logger.info('Program finished')
-
+        get_data = RssParser(arguments.url, arguments.limit)
+        try:
+            get_data.get_feed()
+        except Exception as error_message:
+            logger.error(error_message)
+            if not arguments.verbose:
+                print(error_message)
+        if arguments.json:
+            print(get_data.convert_to_json())
+        else:
+            print(get_data)
+        logger.info(f'Program finished, {len(get_data.items)} news was showed')
 
 if __name__ == '__main__':
     main()
