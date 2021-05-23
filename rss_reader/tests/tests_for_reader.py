210094b4e36c9f3483f0a8f2a11fa5c383b7a936
bobich.m <mbobich@marathonbet.ru>
2021-05-23 22:44:56 +0300

    Correcting tests_for_reader.py

--- rss_reader/tests/tests_for_reader.py
+++ rss_reader/tests/tests_for_reader.py
@@ -6,25 +6,12 @@ import requests
 from unittest.mock import patch
 from unittest.mock import MagicMock
 from io import StringIO
-
 from rss_core.reader import SiteReader
 
 
 class TestReader(unittest.TestCase):
     """ Tests for SiteReader """
 
-    def setUp(self):
-        self.get = requests.get
-
-    def tearDown(self):
-        requests.get = self.get
-
-    def test_bad_response_code(self):
-        response = requests.Response()
-        response.status_code = 404
-        requests.get = MagicMock(return_value=response)
-        reader = SiteReader()
-        self.assertRaises(ConnectionError, reader.get_data, "http://")
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_get_data_empty_link(self, mock_stdout):
