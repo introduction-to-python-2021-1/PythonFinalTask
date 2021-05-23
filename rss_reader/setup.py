529301d86ff632a1b2f9d44b3903e2acd509985d
Eduard <wasanski@mail.ru>
2021-05-23 15:26:12 +0300

    Changing the module hierarchy

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rss_reader',
-    version=1.1,
+    version=2.1,
     description='Pure Python command-line RSS reader',
     author='Eduard Vasanski',
     author_email='wasanski@mail.ru',
@@ -14,7 +14,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'rss_reader = rss_reader.rss_reader.rss_reader:main',
+            'rss_reader = rss_reader.rss_reader:main',
         ]
     },
     install_requires=['requests==2.25.1', 'feedparser==6.0.2'],
