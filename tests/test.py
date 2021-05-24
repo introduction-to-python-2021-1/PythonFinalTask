61b039e3aac9c74df231d5d2d6f31473b6d64820
YuliyaLit <Yuliya-litvinko@mail.ru>
2021-05-24 23:45:49 +0300

    add tests

--- tests/test.py
+++ tests/test.py
@@ -4,12 +4,12 @@ import sys
 import io
 
 class TestReader(unittest.TestCase):
-    def checking_the_installation(self):
+    def test_checking_the_installation(self):
         """Creates object and outputs it to stdout"""
         self.output = io.StringIO()
         sys.stdout = self.output
 
-    def set_limit_for_print(self):
+    def test_set_limit_for_print(self):
         """Good limit"""
         parser = rss_reader.command_arguments_parser(["--limit 3"])
         self.assertTrue(parser)
@@ -19,12 +19,28 @@ class TestReader(unittest.TestCase):
         parser = rss_reader.command_arguments_parser(["--limit 0"])
         self.assertTrue(parser)
 
-    def checking_test_verbose(self):
+    def test_limit_negative_number(self):
+        """Test limit negative number"""
+        parser = rss_reader.command_arguments_parser(["--limit -5"])
+        self.assertTrue(parser)
+
+    def test_checking_verbose(self):
         """Test verbose status message"""
         parser = rss_reader.command_arguments_parser(["https://news.yahoo.com/rss/", "--verbose"])
         self.assertTrue(parser.verbose)
 
-    def checking_empty(self):
+    def test_checking_verbose_plus_limit(self):
+        """Test verbose status message and limit"""
+        parser = rss_reader.command_arguments_parser(["--limit 4", "--verbose"])
+        self.assertTrue(parser.verbose)
+
+    def test_checking_json_format(self):
+        """Test json format"""
+        parser = rss_reader.command_arguments_parser(["https://news.yahoo.com/rss/", "--json"])
+        self.assertTrue(parser.json)
+
+
+    def test_checking_empty(self):
         """Test without link"""
         parser = rss_reader.command_arguments_parser([""])
         self.assertTrue(parser)
@@ -32,4 +48,3 @@ class TestReader(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
