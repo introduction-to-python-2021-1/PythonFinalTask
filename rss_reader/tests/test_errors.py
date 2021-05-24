2f96e032402f240eb864a4ffbf1cbdc94e001942
spectraise <spectraise@gmail.com>
2021-05-25 00:55:48 +0300

    Fixed tests for errors

--- rss_reader/tests/test_errors.py
+++ rss_reader/tests/test_errors.py
@@ -1,4 +1,7 @@
+import argparse
+from io import StringIO
 import unittest
+from unittest.mock import patch
 
 from rss_reader.rss_reader.rss_reader import main
 
@@ -13,21 +16,23 @@ class TestRssReader(unittest.TestCase):
             'ERROR:root: An error occurred while sending a GET request to the specified URL. Check the specified URL'
             ' and your internet connection', cm.output)
 
-    def test_negative_limit(self):
+    @patch('sys.stderr', new_callable=StringIO)
+    def test_negative_limit(self, mock_stderr):
         """Tests that if --limit option is negative app should print error"""
         argv = ['https://news.yahoo.com/rss/', '--limit=-1']
         with self.assertRaises(SystemExit):
-            with self.assertLogs('root', level='ERROR') as cm:
+            with self.assertRaises(argparse.ArgumentTypeError):
                 main(argv)
-        self.assertIn('ERROR:root: The limit argument must be greater than zero (-1 was passed)', cm.output)
+        self.assertIn('The limit argument must be greater than zero (-1 was passed)', mock_stderr.getvalue())
 
-    def test_zero_limit(self):
+    @patch('sys.stderr', new_callable=StringIO)
+    def test_zero_limit(self, mock_stderr):
         """Tests that if --limit option is zero app should print error"""
         argv = ['https://news.yahoo.com/rss/', '--limit=0']
         with self.assertRaises(SystemExit):
-            with self.assertLogs('root', level='ERROR') as cm:
+            with self.assertRaises(argparse.ArgumentTypeError):
                 main(argv)
-        self.assertIn('ERROR:root: The limit argument must be greater than zero (0 was passed)', cm.output)
+        self.assertIn('The limit argument must be greater than zero (0 was passed)', mock_stderr.getvalue())
 
     def test_url_has_no_schema_supplied(self):
         """Tests that if specified URL has no schema supplied app should print error"""
