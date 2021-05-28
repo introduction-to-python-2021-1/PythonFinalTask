78be25c673c596c0e7b442a44a32c8c1f0c92e0f
Viktor Kolesnikov <mack123456789@mail.ru>
2021-05-28 19:38:34 +0300

    Try to fix rss_feed.xml path

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -9,6 +9,9 @@ setup(
     install_requires=['beautifulsoup4', 'lxml', 'requests', 'python-dateutil', ],
     python_requires='>=3.8',
     packages=['rss_reader'],
+    package_data={
+            'components': ['tests/rss_feed.xml']
+        },
     entry_points={
         'console_scripts': [
             'rss_reader=rss_reader.rss_reader:main',
