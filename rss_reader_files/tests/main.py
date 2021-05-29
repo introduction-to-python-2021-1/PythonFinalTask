d4b2e0d92efc501674366836e62b0d9e1a85df0b
vladsheydenkov <65013898+vladsheydenkov@users.noreply.github.com>
2021-05-29 10:35:09 +0300

    Delete rss_reader_files directory

--- rss_reader_files/tests/main.py
+++
@@ -1,8 +0,0 @@
-import os
-import unittest
-
-if __name__ == '__main__':
-    test_loader = unittest.TestLoader()
-    suite = unittest.TestLoader().discover('.', pattern="*_test.py")
-    tests = unittest.TextTestRunner()
-    tests.run(suite)
