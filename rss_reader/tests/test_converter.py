31cd1a50bd9cf916d1bab1db97c515522cee1810
spectraise <spectraise@gmail.com>
2021-06-03 00:02:57 +0300

    Fixed test_to_html

--- rss_reader/tests/test_converter.py
+++ rss_reader/tests/test_converter.py
@@ -1,6 +1,3 @@
-import filecmp
-import os
-
 from bs4 import BeautifulSoup
 
 from rss_reader.tests.testing import BaseTest
@@ -8,11 +5,10 @@ from rss_reader.tests.testing import BaseTest
 
 class TestConverter(BaseTest):
     def test_to_html(self):
-        """Tests that html is generated and saved to a file"""
-        self.converter.to_html([self.example_feed], None, os.path.dirname(__file__) + os.sep + 'test.html')
-        self.assertTrue(
-            filecmp.cmp(os.path.dirname(__file__) + os.sep + 'test.html', self.data_folder + 'result_html.html'))
-        os.remove(os.path.dirname(__file__) + os.sep + 'test.html')
+        """Tests that html is generated"""
+        result_html = self.converter.to_html([self.example_feed], None, pdf=True, output_filepath='test.pdf')
+        document_data = BeautifulSoup(result_html, 'lxml')
+        self.assertEqual(len(document_data.find_all('div')), 5)
 
     def test_to_html_with_limit(self):
         """Tests that the --limit argument affects the number of news in html"""
