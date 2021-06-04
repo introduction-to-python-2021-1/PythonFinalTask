d07d0a94031b791160d60559fe54be4f83e2ea53
UrekMazin0 <rostov.repin99@gmail.com>
2021-06-04 18:09:20 +0300

    setup.py fix

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
     
-with open("LICENSE", "r", encoding="utf-8") as fh
+with open("LICENSE", "r", encoding="utf-8") as fh:
     license = fh.read
 
 setuptools.setup(
@@ -14,7 +14,7 @@ setuptools.setup(
     description="Simpl python command-line RSS-reader",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license = license
+    license = license,
     url="https://github.com/UrekMazin0/PythonFinalTask/tree/main/rss_reader",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -24,7 +24,8 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=["urllib==1.26.4", "argparse==1.4.0", "beautifulsoup4==4.9.3"]
+    install_requires=["urllib==1.26.4", "argparse==1.4.0", "beautifulsoup4==4.9.3",
+                      "pycodestyle==2.4.0", "nose==1.3.7", "termcolor==1.1.0", "coverage==4.5.1"],
     entry_points={
         "console_scripts": [
             'rss_reader=src.reader:main'
