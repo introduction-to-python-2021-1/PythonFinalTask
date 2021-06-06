35b819d986174418b3cd57901475611ed38b8206
spectraise <spectraise@gmail.com>
2021-06-07 00:15:53 +0300

    Fixed requirements

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -12,7 +12,7 @@ setup(
     },
     install_requires=['bs4==0.0.1', 'beautifulsoup4==4.9.3', 'lxml==4.6.3', 'python-dateutil==2.8.1',
                       'requests==2.25.1', 'pathvalidate==2.4.1', 'jinja2==2.11.3', 'xhtml2pdf==0.2.5',
-                      'colorlog==5.0.1', 'Pygments==2.9.0', 'ddt==1.4.2', 'pytest==6.2.4', 'pytest-cov==2.12.1'],
+                      'colorlog==5.0.1', 'Pygments==2.9.0'],
     python_requires='>=3.8',
     entry_points={
         'console_scripts': [
