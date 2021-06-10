868ef1c652b2faf759db5ee12b079b787ea7079f
Eduard <wasanski@mail.ru>
2021-06-10 17:29:31 +0300

    refactor news list

--- rss_reader/tests/data/news_list.py
+++ rss_reader/tests/data/news_list.py
@@ -2,13 +2,13 @@ news = [
     {
         "url": "https://www.rt.com/rss/news/",
         "feed": "RT World News",
-        "title": "China criticizes US for being ‘world’s number one secret stealer’ after reports of NSA spying on allied leaders",
-        "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/?utm_source=rss&utm_medium=rss&utm_campaign=RSS",
+        "title": "China criticizes US for being ‘world’s number one secret stealer’ after reports of NSA spying",
+        "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/",
         "date": "Fri, 04 Jun 2021 11:02:52 +0000",
-        "description": " The Chinese Foreign Ministry has issued a stern rebuke of US intelligence practices after Emmanuel Macron and Angela Merkel demanded answers about reports that Washington used Danish intelligence to monitor its allied leaders. Read Full Article at RT.com",
+        "description": " The Chinese Foreign Ministry has issued a stern rebuke of US intelligence ",
         "links": [
             {
-                "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/?utm_source=rss&utm_medium=rss&utm_campaign=RSS",
+                "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/",
                 "type": "link"
             },
             {
@@ -21,12 +21,12 @@ news = [
         "url": "https://www.rt.com/rss/news/",
         "feed": "RT World News",
         "title": "Singapore uses special access route to fast-track use of China’s Sinovac Covid vaccine",
-        "link": "https://www.rt.com/news/525484-singapore-special-access-sinovac-covid-vaccine/?utm_source=rss&utm_medium=rss&utm_campaign=RSS",
+        "link": "https://www.rt.com/news/525484-singapore-special-access-sinovac-covid-vaccine/",
         "date": "Wed, 02 Jun 2021 14:32:59 +0000",
-        "description": " Singapore’s health ministry has announced China’s Sinovac Covid-19 vaccine has been cleared for use under the city-state’s special access route, after the WHO added the jab to its emergency use listing. Read Full Article at RT.com",
+        "description": " Singapore’s health ministry has announced China’s Sinovac Covid-19 vaccine has been cleared",
         "links": [
             {
-                "link": "https://www.rt.com/news/525484-singapore-special-access-sinovac-covid-vaccine/?utm_source=rss&utm_medium=rss&utm_campaign=RSS",
+                "link": "https://www.rt.com/news/525484-singapore-special-access-sinovac-covid-vaccine/",
                 "type": "link"
             },
             {
@@ -41,14 +41,14 @@ news = [
         "title": "Основатель NEXTA отреагировал на признания Протасевича в интервью",
         "link": "https://lenta.ru/news/2021/06/04/pitki/",
         "date": "Fri, 04 Jun 2021 13:13:00 +0300",
-        "description": "Громкие признания бывшего главного редактора Telegram-канала NEXTA Романа Протасевича, сделанные в ходе интервью государственному белорусскому телеканалу ОНТ, стали результатом невыносимых пыток, психологического давления и воздействия медикаментов, считает основатель NEXTA Степан Путило",
+        "description": "Громкие признания бывшего главного редактора Telegram-канала NEXTA Романа Протасевича",
         "links": [
             {
                 "link": "https://lenta.ru/news/2021/06/04/pitki/",
                 "type": "link"
             },
             {
-                "link": "https://icdn.lenta.ru/images/2021/06/04/13/20210604130040291/pic_bb92af3fd4a0d58b675fbccf9e3c99ca.jpg",
+                "link": "https://icdn.lenta.ru/images/2021.jpg",
                 "type": "image"
             }
         ]
