409733d017ba1b00e3f0cfb2571130ec15e3bf29
Eduard <wasanski@mail.ru>
2021-06-11 13:31:15 +0300

    added tests

--- rss_reader/tests/data/news_list.py
+++ rss_reader/tests/data/news_list.py
@@ -2,17 +2,17 @@ news = [
     {
         "url": "https://www.rt.com/rss/news/",
         "feed": "RT World News",
-        "title": "China criticizes US for being ‘world’s number one secret stealer’ after reports of NSA spying",
-        "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/",
+        "title": "title1",
+        "link": "https://www.url1.com/",
         "date": "Fri, 04 Jun 2021 11:02:52 +0000",
-        "description": " The Chinese Foreign Ministry has issued a stern rebuke of US intelligence ",
+        "description": "description1",
         "links": [
             {
-                "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/",
+                "link": "https://www.url1.com",
                 "type": "link"
             },
             {
-                "link": "https://cdni.rt.com/files/2021.06/thumbnail/60b8b5b620302731b846dd9b.JPG",
+                "link": "https://www.url1.com/image",
                 "type": "image"
             }
         ]
@@ -20,17 +20,16 @@ news = [
     {
         "url": "https://www.rt.com/rss/news/",
         "feed": "RT World News",
-        "title": "Singapore uses special access route to fast-track use of China’s Sinovac Covid vaccine",
-        "link": "https://www.rt.com/news/525484-singapore-special-access-sinovac-covid-vaccine/",
+        "title": "title2",
+        "link": "https://www.url2.com/",
         "date": "Wed, 02 Jun 2021 14:32:59 +0000",
-        "description": " Singapore’s health ministry has announced China’s Sinovac Covid-19 vaccine has been cleared",
         "links": [
             {
-                "link": "https://www.rt.com/news/525484-singapore-special-access-sinovac-covid-vaccine/",
+                "link": "https://www.url2.com",
                 "type": "link"
             },
             {
-                "link": "https://cdni.rt.com/files/2021.06/thumbnail/60b7928285f540162222e812.JPG",
+                "link": "https://www.url2.com/image",
                 "type": "image"
             }
         ]
@@ -38,19 +37,103 @@ news = [
     {
         "url": "https://lenta.ru/rss/news",
         "feed": "Lenta.ru : Новости",
-        "title": "Основатель NEXTA отреагировал на признания Протасевича в интервью",
-        "link": "https://lenta.ru/news/2021/06/04/pitki/",
+        "title": "title3",
+        "link": "https://www.url3.com/",
         "date": "Fri, 04 Jun 2021 13:13:00 +0300",
-        "description": "Громкие признания бывшего главного редактора Telegram-канала NEXTA Романа Протасевича",
-        "links": [
-            {
-                "link": "https://lenta.ru/news/2021/06/04/pitki/",
-                "type": "link"
-            },
-            {
-                "link": "https://icdn.lenta.ru/images/2021.jpg",
-                "type": "image"
-            }
-        ]
-    },
+        "description": "description2"
+    }
 ]
+
+output = """
+Feed: RT World News
+
+Title: title1
+Date: Fri, 04 Jun 2021 11:02:52 +0000
+Link: https://www.url1.com/
+
+description1
+
+Links:
+[1] https://www.url1.com (link)
+[2] https://www.url1.com/image (image)
+
+Feed: RT World News
+
+Title: title2
+Date: Wed, 02 Jun 2021 14:32:59 +0000
+Link: https://www.url2.com/
+
+Links:
+[1] https://www.url2.com (link)
+[2] https://www.url2.com/image (image)
+
+Feed: Lenta.ru : Новости
+
+Title: title3
+Date: Fri, 04 Jun 2021 13:13:00 +0300
+Link: https://www.url3.com/
+
+description2
+
+"""
+
+output_json = """[
+   {
+      "url": "https://www.rt.com/rss/news/",
+      "feed": "RT World News",
+      "title": "title1",
+      "link": "https://www.url1.com/",
+      "date": "Fri, 04 Jun 2021 11:02:52 +0000",
+      "description": "description1",
+      "links": [
+         {
+            "link": "https://www.url1.com",
+            "type": "link"
+         },
+         {
+            "link": "https://www.url1.com/image",
+            "type": "image"
+         }
+      ]
+   },
+   {
+      "url": "https://www.rt.com/rss/news/",
+      "feed": "RT World News",
+      "title": "title2",
+      "link": "https://www.url2.com/",
+      "date": "Wed, 02 Jun 2021 14:32:59 +0000",
+      "links": [
+         {
+            "link": "https://www.url2.com",
+            "type": "link"
+         },
+         {
+            "link": "https://www.url2.com/image",
+            "type": "image"
+         }
+      ]
+   },
+   {
+      "url": "https://lenta.ru/rss/news",
+      "feed": "Lenta.ru : Новости",
+      "title": "title3",
+      "link": "https://www.url3.com/",
+      "date": "Fri, 04 Jun 2021 13:13:00 +0300",
+      "description": "description2"
+   }
+]
+"""
+
+output_first = """
+Feed: RT World News
+
+Title: title1
+Date: Fri, 04 Jun 2021 11:02:52 +0000
+Link: https://www.url1.com/
+
+description1
+
+Links:
+[1] https://www.url1.com (link)
+[2] https://www.url1.com/image (image)
+"""
--- rss_reader/tests/test_output.py
+++ rss_reader/tests/test_output.py
@@ -2,44 +2,9 @@ from io import StringIO
 from unittest import TestCase
 from unittest.mock import Mock, patch
 
+from rss_reader.rss_reader.rss_reader import create_logger
 from modules.output import ConsoleOutput
-from tests.data.news_list import news
-
-output = """
-Feed: RT World News
-
-Title: China criticizes US for being ‘world’s number one secret stealer’ after reports of NSA spying
-Date: Fri, 04 Jun 2021 11:02:52 +0000
-Link: https://www.rt.com/news/525559-china-us-france-germany-spies/
-
- The Chinese Foreign Ministry has issued a stern rebuke of US intelligence 
-
-Links:
-[1] https://www.rt.com/news/525559-china-us-france-germany-spies/ (link)
-[2] https://cdni.rt.com/files/2021.06/thumbnail/60b8b5b620302731b846dd9b.JPG (image)
-"""
-
-output_json = """[
-   {
-      "url": "https://www.rt.com/rss/news/",
-      "feed": "RT World News",
-      "title": "China criticizes US for being ‘world’s number one secret stealer’ after reports of NSA spying",
-      "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/",
-      "date": "Fri, 04 Jun 2021 11:02:52 +0000",
-      "description": " The Chinese Foreign Ministry has issued a stern rebuke of US intelligence ",
-      "links": [
-         {
-            "link": "https://www.rt.com/news/525559-china-us-france-germany-spies/",
-            "type": "link"
-         },
-         {
-            "link": "https://cdni.rt.com/files/2021.06/thumbnail/60b8b5b620302731b846dd9b.JPG",
-            "type": "image"
-         }
-      ]
-   }
-]
-"""
+from tests.data.news_list import news, output, output_json, output_first
 
 
 class TestConsoleOutput(TestCase):
@@ -49,10 +14,28 @@ class TestConsoleOutput(TestCase):
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_output(self, mock_stdout):
-        self.output.output(news, limit=1)
+        self.output.output(news, limit=False)
         self.assertEqual(mock_stdout.getvalue(), output)
 
     @patch('sys.stdout', new_callable=StringIO)
+    def test_output_with_negative_limit(self, mock_stdout):
+        self.output.output(news, limit=-999)
+        self.assertEqual(mock_stdout.getvalue(), output)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_output_with_limit(self, mock_stdout):
+        self.output.output(news, limit=1)
+        self.assertEqual(mock_stdout.getvalue(), output_first)
+
+    @patch('sys.stdout', new_callable=StringIO)
     def test_output_json(self, mock_stdout):
-        self.output.output_json(news, limit=1)
+        self.output.output_json(news, limit=False)
         self.assertEqual(mock_stdout.getvalue(), output_json)
+
+    def test_output_manager(self):
+        with self.assertLogs(logger='root', level='DEBUG') as logs:
+            with ConsoleOutput(logger=create_logger()):
+                pass
+        self.assertIn('DEBUG:root:News printing has started.', logs.output)
+        self.assertIn('DEBUG:root:News printing has finished.', logs.output)
+
