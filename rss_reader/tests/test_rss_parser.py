21021cca393a39f533d2048922f9f21ec57d07fe
Yarshou <dershov03@yandex.by>
2021-06-14 00:16:56 +0300

    Refactored + added a few more tests

--- rss_reader/tests/test_rss_parser.py
+++ rss_reader/tests/test_rss_parser.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from rss_reader.src.modules.rss_parser import RSSParser
 from rss_reader.src.rss_reader import create_logger
 
 rss_channel = """
@@ -37,89 +36,33 @@ rss_channel = """
     </channel>
 </rss>"""
 
-result = r"""{'bozo': 1,
- 'bozo_exception': SAXParseException('not well-formed (invalid token)'),
- 'encoding': 'utf-8',
- 'entries': [{'credit': '',
-              'guidislink': False,
-              'id': 'undervaccinated-red-states-are-nowhere-near-herd-immunity-as-\n'
-                    '            '
-                    'dangerous-delta-variant-spreads-090052183.html',
-              'link': 'https://news.yahoo.com/undervaccinated-red-states-are-nowhere-near-herd-immunity-as-dangerous-\n'
-                      '                  delta-variant-spreads-090052183.html',
-              'links': [{'href': 'https://news.yahoo.com/undervaccinated-red-states-are-nowhere-near-herd-immunity-as-dangerous-\n'
-                                 '                  '
-                                 'delta-variant-spreads-090052183.html',
-                         'rel': 'alternate',
-                         'type': 'text/html'}],
-              'media_content': [{'height': '86',
-                                 'url': 'https://s.yimg.com/os/creatr-uploaded-images/2021-06/1393bb40-ca2d-\n'
-                                        '            11eb-bddf-ea9188da9f73',
-                                 'width': '130'}],
-              'media_credit': [{'role': 'publishing company'}],
-              'published': '2021-06-11T09:00:52Z',
-              'published_parsed': time.struct_time(tm_year=2021, tm_mon=6, tm_mday=11, tm_hour=9, tm_min=0, tm_sec=52, tm_wday=4, tm_yday=162, tm_isdst=0),
-              'source': {'href': 'https://news.yahoo.com/',
-                         'title': 'Yahoo News'},
-              'title': 'Undervaccinated red states are nowhere near herd '
-                       'immunity as dangerous Delta variant spreads',
-              'title_detail': {'base': '',
-                               'language': None,
-                               'type': 'text/plain',
-                               'value': 'Undervaccinated red states are '
-                                        'nowhere near herd immunity as '
-                                        'dangerous Delta variant spreads'}}],
- 'feed': {'image': {'href': 'https://l.yimg.com/rz/d/yahoo_news_en-US_s_f_p_168x21_news.png',
-                    'link': 'https://www.yahoo.com/news',
-                    'links': [{'href': 'https://www.yahoo.com/news',
-                               'rel': 'alternate',
-                               'type': 'text/html'}],
-                    'title': 'Yahoo News - Latest News & Headlines',
-                    'title_detail': {'base': '',
-                                     'language': None,
-                                     'type': 'text/plain',
-                                     'value': 'Yahoo News - Latest News & '
-                                              'Headlines'}},
-          'language': 'en-US',
-          'link': 'https://www.yahoo.com/news',
-          'links': [{'href': 'https://www.yahoo.com/news',
-                     'rel': 'alternate',
-                     'type': 'text/html'}],
-          'published': 'Fri, 11 Jun 2021 09:22:10 -0400',
-          'published_parsed': time.struct_time(tm_year=2021, tm_mon=6, tm_mday=11, tm_hour=13, tm_min=22, tm_sec=10, tm_wday=4, tm_yday=162, tm_isdst=0),
-          'rights': 'Copyright (c) 2021 Yahoo! Inc. All rights reserved',
-          'rights_detail': {'base': '',
-                            'language': None,
-                            'type': 'text/plain',
-                            'value': 'Copyright (c) 2021 Yahoo! Inc. All '
-                                     'rights reserved'},
-          'subtitle': 'The latest news and headlines from Yahoo! News. Get '
-                      'breaking news stories and in-depth coverage\n'
-                      '         with videos and photos.',
-          'subtitle_detail': {'base': '',
-                              'language': None,
-                              'type': 'text/html',
-                              'value': 'The latest news and headlines from '
-                                       'Yahoo! News. Get breaking news stories '
-                                       'and in-depth coverage\n'
-                                       '         with videos and photos.'},
-          'title': 'Yahoo News - Latest News & Headlines',
-          'title_detail': {'base': '',
-                           'language': None,
-                           'type': 'text/plain',
-                           'value': 'Yahoo News - Latest News & Headlines'},
-          'ttl': '5'},
- 'headers': {},
- 'namespaces': {'media': 'https://search.yahoo.com/mrss/'},
- 'version': 'rss20'}"""
-
 
 class TestRssParser(TestCase):
 
     def setUp(self) -> None:
         self.logger = create_logger()
 
-    def test_get_rss_channel(self):
-        channel = RSSParser(rss_channel, self.logger, 1).get_rss_channel()
-        self.assertEqual(channel, result)
-
+    @patch('src.modules.rss_parser.RSSParser')
+    def test_parse(self, MockRSSParser):
+        rss_parser = MockRSSParser(rss_channel, self.logger, 1)
+        rss_parser.parse.return_value = [
+            {
+                'new 0': {
+                    'feed': 'Yahoo News - Latest News & Headlines',
+                    'title': 'Undervaccinated red states are nowhere near herd immunity as dangerous Delta variant'
+                             ' spreads',
+                    'date': '2021-06-11T09:00:52Z',
+                    'link': 'https://news.yahoo.com/undervaccinated-red-states-are-nowhere-near-herd-immunity-as'
+                            '-dangerous-delta-variant-spreads-090052183.html',
+                    'links': {
+                        'link 0': {
+                            'href': 'https://news.yahoo.com/undervaccinated-red-states-are-nowhere-near-herd-'
+                                    'immunity-as-dangerous-delta-variant-spreads-090052183.html',
+                            'type': 'text/html'
+                        }
+                    }
+                }
+            }
+        ]
+        response = rss_parser.parse()
+        self.assertEqual(response, rss_parser.parse.return_value)
--- rss_reader/tests/test_url_validator.py
+++ rss_reader/tests/test_url_validator.py
@@ -13,3 +13,19 @@ class TestUrlValidator(TestCase):
         validator = RssUrlValidator('https://lenta.ru/rss/news/', self.logger)
         status_code = validator.get_status_code()
         self.assertEqual(status_code, 200)
+
+    def test_get_status_code_failure(self):
+        status_code_1 = RssUrlValidator('https://lenta.ru/rss/nes/', self.logger).get_status_code()
+        self.assertEqual(status_code_1, 404)
+        status_code_2 = RssUrlValidator('htt://lenta.ru/rss/news/', self.logger).get_status_code()
+        self.assertEqual(status_code_2, None)
+        status_code_3 = RssUrlValidator('', self.logger).get_status_code()
+        self.assertEqual(status_code_3, None)
+
+    def test_get_validated_url_success(self):
+        url = RssUrlValidator('https://lenta.ru/rss/news/', self.logger).get_validated_url()
+        self.assertEqual(url, 'https://lenta.ru/rss/news/')
+
+    def test_get_validated_url_failure(self):
+        url = RssUrlValidator('https://lenta.u/rss/news/', self.logger).get_validated_url()
+        self.assertEqual(url, '')
