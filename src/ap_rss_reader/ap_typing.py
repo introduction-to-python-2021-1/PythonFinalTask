e669500c9c24634d190b832d00de5b9121313846
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-06-13 18:52:30 +0300

    feat: add additional types to ap_typing.py
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/ap_rss_reader/ap_typing.py
+++ src/ap_rss_reader/ap_typing.py
@@ -2,8 +2,34 @@
 
 from __future__ import annotations
 
+from datetime import datetime
 from typing import Callable
+from typing import List
+from typing import Literal
+from typing import Optional
+from typing import Tuple
+from typing import Union
+
+from bs4 import Tag  # type: ignore
 
 from ap_rss_reader.ap_collections import Article
+from ap_rss_reader.ap_collections import Media
 
+FieldName = Literal[
+    "title",
+    "link",
+    "description",
+    "author",
+    "category",
+    "comments",
+    "enclosure",
+    "media",
+    "media_content",
+    "media_thumbnail",
+    "pubdate",
+    "source",
+]
+FieldValue = Optional[Union[datetime, str, List[Media], List[str]]]
+FieldParser = Callable[[Tag, FieldName], Tuple[FieldName, FieldValue]]
+FieldPrinter = Callable[[Article, FieldName], None]
 Filter = Callable[[Article], bool]
