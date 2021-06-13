1993137a2b35244759958d14c62bb6121d0315bf
aplatkouski <5857672+aplatkouski@users.noreply.github.com>
2021-06-13 18:18:12 +0300

    feat: add 'Media' class to ap_collections.py
    Signed-off-by: aplatkouski <5857672+aplatkouski@users.noreply.github.com>

--- src/ap_rss_reader/ap_collections.py
+++ src/ap_rss_reader/ap_collections.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from datetime import datetime
 from typing import NamedTuple
+from typing import Optional
 
 
 class Article(NamedTuple):
@@ -15,3 +16,12 @@ class Article(NamedTuple):
     source: str
     source_url: str
     media_content_url: str
+
+
+class Media(NamedTuple):
+    """The media content of rss article."""
+
+    type: str
+    url: str
+    height: Optional[str]
+    width: Optional[str]
