e88dfb1f27222973ed550f705d69024522e39689
byjk <jk@tut.by>
2021-06-14 22:19:30 +0300

    changed coverage and addev -v command line argument

--- rss_reader/rss_reader/args.py
+++ rss_reader/rss_reader/args.py
@@ -8,7 +8,7 @@ class RssReaderArgs:
         self.__parser = argparse.ArgumentParser(
             prog='rss_reader', description='Pure Python command-line RSS reader.')
         self.__parser.add_argument(
-            '--version', action='version', version='%(prog)s 1.2', help='Print version info')
+            '-v', '--version', action='version', version='%(prog)s 1.2', help='Print version info')
         self.__parser.add_argument(
             '--json', default=False, action='store_true', help='Print result as JSON in stdout')
         self.__parser.add_argument(
