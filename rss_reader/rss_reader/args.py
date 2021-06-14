7d175dac457a4064d5b75df8389e31658505f230
byjk <jk@tut.by>
2021-06-14 22:06:06 +0300

    added tests

--- rss_reader/rss_reader/args.py
+++ rss_reader/rss_reader/args.py
@@ -3,35 +3,19 @@ import argparse
 
 class RssReaderArgs:
     """class for command line arguments parsing"""
-    def __init__(self):
-        self.__parser = argparse.ArgumentParser(prog='rss_reader', description='Pure Python command-line RSS reader.')
-        self.__parser.add_argument(
-            '--version',
-            # '-v',
-            action='version',
-            version='%(prog)s 1.2',
-            help='Print version info')
-        self.__parser.add_argument(
-            '--json',
-            default=False,
-            action='store_true',
-            help='Print result as JSON in stdout'
-        )
-        self.__parser.add_argument(
-            '--verbose',
-            default=False,
-            action='store_true',
-            help='Outputs verbose status messages'
-        )
-        self.__parser.add_argument(
-            '--limit',
-            type=int,
-            help='Limit news topics if this parameter provided')
-        self.__parser.add_argument(
-            'source',
-            # nargs=1,
-            type=str,
-            help='RSS URL'
-        )
 
+    def __init__(self):
+        self.__parser = argparse.ArgumentParser(
+            prog='rss_reader', description='Pure Python command-line RSS reader.')
+        self.__parser.add_argument(
+            '--version', action='version', version='%(prog)s 1.2', help='Print version info')
+        self.__parser.add_argument(
+            '--json', default=False, action='store_true', help='Print result as JSON in stdout')
+        self.__parser.add_argument(
+            '--verbose', default=False, action='store_true', help='Outputs verbose status messages')
+        self.__parser.add_argument(
+            '--limit', type=int, help='Limit news topics if this parameter provided')
+        self.__parser.add_argument(
+            'source', type=str, help='RSS URL'
+        )
         self.args = self.__parser.parse_args()
--- rss_reader/tests/test_reader.py
+++ rss_reader/tests/test_reader.py
@@ -11,3 +11,25 @@ def test_reader():
     assert entries is not None
     assert len(entries) == 2
     pass
+
+
+def test_reader_print():
+    app = RssReaderApp()
+    path = os.path.dirname(__file__)
+    sample = os.path.join(path, 'sample.xml')
+    entries = app.rss_reader(sample, limit=2)
+    assert entries is not None
+    assert len(entries) == 2
+    app.print_entries(entries, json_fmt=False)
+    pass
+
+
+def test_reader_print_json():
+    app = RssReaderApp()
+    path = os.path.dirname(__file__)
+    sample = os.path.join(path, 'sample.xml')
+    entries = app.rss_reader(sample, limit=2)
+    assert entries is not None
+    assert len(entries) == 2
+    app.print_entries(entries, json_fmt=True)
+    pass
