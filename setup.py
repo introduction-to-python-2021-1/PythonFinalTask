f29ae8feaf393486dff09894f6758d61ee2d5197
vokasi1417 <vokasi1417@gmail.com>
2021-06-15 22:15:45 +0300

    second iteration

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="rss_reader",
+    version="2.1",
+    description="Pure Python RSS reader",
+    url="https://github.com/jesuisbourrasque/PythonFinalTask",
+    author="Nikita Isakov",
+    author_email="jesuisbourrasque@gmail.com",
+    packages=find_packages(include=["rss_reader"]),
+    install_requires=required,
+    entry_points={"console_scripts": ["rss-reader=rss_reader.main:main"]},
+)
