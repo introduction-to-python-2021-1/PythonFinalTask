ff089d15679f1936fdf1bea458df0761875cd44f
vokasi1417 <vokasi1417@gmail.com>
2021-06-19 13:29:46 +0300

    try to fix CI

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup, find_packages
 
 
-with open("../requirements.txt") as f:
-    required = f.read().splitlines()
-
 setup(
     name="rss_reader",
     version="4.1",
@@ -12,6 +9,16 @@ setup(
     author="Nikita Isakov",
     author_email="jesuisbourrasque@gmail.com",
     packages=find_packages(include=["rss_reader"]),
-    install_requires=required,
+    install_requires=[
+        "feedparser==6.0.2",
+        "pytest==6.2.4",
+        "pytest-mock==3.6.1",
+        "pytest-cov==2.12.1"
+        "python-dateutil==2.8.1",
+        "requests==2.25.1",
+        "fpdf2==2.4.1",
+        "Pillow==8.2.0",
+        "jinja2==3.0.1",
+                      ],
     entry_points={"console_scripts": ["rss-reader=rss_reader.main:main"]},
 )
