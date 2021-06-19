7763a5a7b56b5b54db5716e18c1d85a24f8ecabd
vokasi1417 <vokasi1417@gmail.com>
2021-06-19 14:28:10 +0300

    try to fix CI

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ setup(
     author_email="jesuisbourrasque@gmail.com",
     packages=find_packages(include=["rss_reader"]),
     install_requires=[
+        "pycodestyle==2.4.0",
+        "nose==1.3.7",
+        "coverage==5.5",
+        "termcolor==1.1.0"
         "feedparser==6.0.2",
         "pytest==6.2.4",
         "pytest-mock==3.6.1",
