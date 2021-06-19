b5bab22b288e7fd15b83543379b11b12be996d7f
vokasi1417 <vokasi1417@gmail.com>
2021-06-19 13:32:58 +0300

    try to fix CI

--- rss_reader/setup.py
+++ rss_reader/setup.py
@@ -13,7 +13,7 @@ setup(
         "feedparser==6.0.2",
         "pytest==6.2.4",
         "pytest-mock==3.6.1",
-        "pytest-cov==2.12.1"
+        "pytest-cov==2.12.1",
         "python-dateutil==2.8.1",
         "requests==2.25.1",
         "fpdf2==2.4.1",
